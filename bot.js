a0c100e1088821919c84b5b47897eee62c49975a
Ubuntu <ubuntu@ip-172-31-16-211.us-east-2.compute.internal>
2021-11-13 23:11:27 +0000

    add aws

--- bot.js
+++ bot.js
@@ -4,8 +4,13 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+const AWS = require('aws-sdk');
+var config = new AWS.Config({ region: 'us-east-2' });
+var ec2 = new AWS.EC2();
+
 client.on('ready', () => {
    console.log(`Logged in as ${client.user.tag}!`);
+   console.log("Access key:", AWS.config.credentials.accessKeyId);
 })
 
 client.on("messageCreate", msg => {
