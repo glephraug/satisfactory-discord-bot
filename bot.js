ff0815cd6b9a0dcef3852a06f87ee5abe35940b3
Craig Cambias <craig.cc@gmail.com>
2021-11-13 23:49:14 +0000

    connect to aws

--- bot.js
+++ bot.js
@@ -5,20 +5,32 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const AWS = require('aws-sdk');
-var config = new AWS.Config({ region: 'us-east-2' });
 var ec2 = new AWS.EC2();
 
 client.on('ready', () => {
    console.log(`Logged in as ${client.user.tag}!`);
-   console.log("Access key:", AWS.config.credentials.accessKeyId);
 })
 
 client.on("messageCreate", msg => {
    if (msg.content === "!factory-status") {
-      msg.reply("I have no idea");
+      var params = { InstanceIds: [ process.env.INSTANCE_ID ] };
+      ec2.describeInstanceStatus(params, function(err, data) {
+	 if (err) {
+            console.log(err, err.stack);
+            msg.reply("Error getting instance status. Sorry.");
+	 } else {
+            if (data.InstanceStatuses.length < 1) {
+               msg.reply("Satisfactory server is down");
+            } else {
+               msg.reply("Satisfactory server is " + data.InstanceStatuses[0].InstanceState.Name);
+            }
+         }
+      });
    } else if (msg.content === "!satisfactory") {
+      console.log("Starting ec2 server")
       msg.reply("Start it yourself");
    } else if (msg.content === "!unsatisfactory") {
+      console.log("Killing ec2 server")
       msg.reply("Not my problem");
    }
 })
