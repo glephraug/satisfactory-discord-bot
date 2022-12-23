509811576d4cf088afb68b19367ba36630354f6b
Craig Cambias <craig.cc@gmail.com>
2022-12-23 02:20:57 -0500

    add service starting

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ var fs = require('fs')
 
 var params = { InstanceIds: [process.env.INSTANCE_ID] };
 
-function server_status(msg) {
+function server_status(msg, text, greet) {
    var ssh = new SSH({
       host: '3.19.154.252',
       user: 'ubuntu',
@@ -31,9 +31,18 @@ function server_status(msg) {
          data += stdout;
       },
       exit: function (code) {
-         msg.reply(data)
+         msg.reply(text + data + " " + greet)
       }
    }).start();
+}
+
+function server_run(name) {
+   var ssh = new SSH({
+      host: '3.19.154.252',
+      user: 'ubuntu',
+      key: fs.readFileSync('/home/ubuntu/discord-control.pem')
+   });
+   ssh.exec("sh /home/ubuntu/aws-game-server/run.sh " + name).start();
 }
 
 client.on('ready', () => {
@@ -46,33 +55,32 @@ client.on("messageCreate", msg => {
       greet = greets.get(msg.author.username);
    }
    if (msg.content === "!server-status") {
-      ec2.describeInstanceStatus(params, function(err, data) {
+      ec2.describeInstanceStatus(params, function (err, data) {
          if (err) {
             console.log(err, err.stack);
             msg.reply("Error getting instance status. Sorry.");
          } else {
             if (data.InstanceStatuses.length < 1) {
-               msg.reply("Satisfactory server is down. " + greet);
+               msg.reply("Server is down. " + greet);
             } else {
-               msg.reply("Satisfactory server is " + data.InstanceStatuses[0].InstanceState.Name + ". " + greet);
-               server_status(msg)
+               server_status(msg, "Server is " + data.InstanceStatuses[0].InstanceState.Name + ". ", greet)
             }
          }
       });
-   } else if (msg.content === "!satisfactory") {
+   } else if (msg.content === "!server-start") {
       console.log("Starting ec2 server");
-      ec2.startInstances(params, function(err, data) {
+      ec2.startInstances(params, function (err, data) {
          if (err) {
             console.log(err, err.stack);
             msg.reply("An error occured when starting the server! Please contact nobody@cares.com");
          } else {
             console.log("Success!");
-            msg.reply("The server is starting up. Have a fun time! " + greet);
+            msg.reply("The server is starting up. Please choose a game. Have a fun time! " + greet);
          }
       });
-   } else if (msg.content === "!unsatisfactory") {
+   } else if (msg.content === "!server-stop") {
       console.log("Killing ec2 server");
-      ec2.stopInstances(params, function(err, data) {
+      ec2.stopInstances(params, function (err, data) {
          if (err) {
             console.log(err, err.stack);
             msg.reply("An error occured when trying to stop the server");
@@ -81,6 +89,12 @@ client.on("messageCreate", msg => {
             msg.reply("The server is off. You're so thoughtful! " + greet);
          }
       });
+   } else if (msg.content === "!server-satisfactory") {
+      msg.reply("Oh ho, you wanna make them items? " + greet);
+      server_run("satisfactory");
+   } else if (msg.content === "!server-valheim") {
+      msg.reply("How soft your fields so green can whisper tales of gore. " + greet);
+      server_run("valheim");
    } else if (msg.content === "fat") {
       msg.reply("no u");
    } else if (msg.content === "luv u") {
